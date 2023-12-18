070469c76621198f2f1f9dc0023bd08ab4df8524
BiliOrange <90908765+orangeZSCB@users.noreply.github.com>
2023-12-18 21:03:54 +0800

    OrangeOJ Uploaded

---
+++ OJ/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'OrangeOJ服务状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm795958405-0544c5481feaf9225d030138',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'OJ台湾节点',
+      url: 'https://oj.zsccodelnc.cn:11451'
+    },
+  ],
+};
