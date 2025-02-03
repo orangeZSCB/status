ca964a83cc4b703bc9ae191bcb6916557bb05b3a
Orange <90908765+orangeZSCB@users.noreply.github.com>
2025-02-03 12:14:27 +0800

    Create config.js

---
+++ MC/config.js
@@ -0,0 +1,30 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'OrangeOJ服务状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm798495493-3650a8724658a9da234bc5eb',
+    'm798495496-465962a9deb359286c2d3bdb',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'OJ 反代主域名',
+      url: 'https://zsccodelnc.cn'
+    },
+    {
+      text: 'OJ 根域',
+      url: 'https://dc82d2c3-e027-c787-ee70-22b4dff0f031.ofalias.com:19198/'
+    },
+  ],
+};
